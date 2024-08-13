059b2934b9f4722bd1bce7075d6b8e65356715a0
Calvin Wang <wangjunneil@gmail.com>
2024-08-13 12:36:22 +0000

    fix title

--- config/index.ts
+++ config/index.ts
@@ -3,8 +3,8 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: 'Translate Assistant',
+  description: 'Calvin Wang\'s AI Translate Assistant',
   copyright: '',
   privacy_policy: '',
   default_language: 'en',
